280f5d13e6d82a1803c9ee300492e6203cd65ceb
iachieve <mhalim80@hotmail.com>
2020-03-27 13:37:40 -0700

    update projects

--- scripts/email.js
+++ scripts/email.js
@@ -1,22 +1,25 @@
 var form_id = "mailForm";
-var data = {"access_token": "li8rptu86iejqdcvbmbek0ec"};
+var sendButton = $("#mailForm-send");
+
+var data = { "access_token": "li8rptu86iejqdcvbmbek0ec" };
+
 function onSuccess() {
-  window.location = window.location.href;
-  alert('Thank you for your message. You will receive a response within 24 hours. Have a great day.')
+  $("#" + form_id).trigger("reset");
+  sendButton.val('Sending…');
+  sendButton.prop('disabled', true);
+  alert('Thank you so much for your message. I will come back to you ASAP. Have a great day.')
 }
 
 function onError(error) {
   alert('error happened')
 }
 
-var sendButton = $("#mailForm-send");
-
 function send() {
-  var name = $("#name").val();
-  var email = $("email").val();
-  var phone = $("#phone").val();
-  var subject = $("#subject").val();
-  var message = $("#message").val();
+  const name = $("#name").val();
+  const email = $("#email").val();
+  const phone = $("#phone").val();
+  const subject = $("#subject").val();
+  const message = $("#message").val();
   if (!subject || !message || !validateCaptcha()) {
     return false;
   }
