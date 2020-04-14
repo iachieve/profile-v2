8cd8e09bc1324ce6d40fc329e92522ddf42ebf09
iachieve <mhalim80@hotmail.com>
2020-04-14 10:33:30 -0700

    add article

--- articles/DS/problems/medium/001/code/reverseInParentheses.js
+++ articles/DS/problems/medium/001/code/reverseInParentheses.js
@@ -1,5 +1,20 @@
-function reverseInParentheses(inputString) {
-  
+// solution one
+function reverseInParentheses(str) {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ')') {
+      stack.push(str[i]);
+    } else {
+      let temp = '';
+      let pop_char = stack.pop();
+      console.log('stack', stack)
+      while (pop_char != '(') {
+        temp += pop_char;
+        pop_char = stack.pop();
+      }
+      temp = temp.split('');
+      stack.push(...temp);
+    }
+  }
+  return stack.join('');
 }
-let inputString = "foo(bar)baz";
-console.log(reverseInParentheses(inputString)) // "foorabbaz")
